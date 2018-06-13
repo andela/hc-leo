383311213ab9987bead8d595f73f279c994f7c90
cosmas augustino billa <cosmas@cosmass-MacBook-Pro.local>
2018-06-13 10:16:20 +0300

    feat: resolve send alert method on Check model
    - ensure up, down and nag alert are sent
    
    [#157694798]

--- hc/api/schemas.py
+++ hc/api/schemas.py
@@ -4,7 +4,6 @@ check = {
         "tags": {"type": "string"},
         "timeout": {"type": "number", "minimum": 60, "maximum": 604800},
         "grace": {"type": "number", "minimum": 60, "maximum": 604800},
-        "nagging_interval": {"type": "number", "minimum": 60, "maximum": 604800},
         "channels": {"type": "string"}
     }
 }
