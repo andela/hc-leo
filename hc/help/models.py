4ea2bef00339ccdfd55f8ceca7fea9527a828a60
victorjambo <victorjambo@live.com>
2018-06-19 15:17:09 +0300

    [feature #157694805] Add FAQ content
    - update readme and give instructions on how to loaddata
    - add a media column to faqs table and makemigrations
    - update html templates to add media on html page

--- hc/help/models.py
+++ hc/help/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Faq(models.Model):
     question = models.CharField(max_length=500)
     answer = models.CharField(max_length=500)
+    media = models.CharField(max_length=500, null=True)
 
     def __str__(self):
         return self.question
