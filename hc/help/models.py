8a2f767ec5fc7dfd951c54492cfff14c66811129
victorjambo <victorjambo@live.com>
2018-06-19 18:12:05 +0300

    [fixes #157694805] fix migrations
    - set media column to blank true
    - add more content

--- hc/help/models.py
+++ hc/help/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Faq(models.Model):
     question = models.CharField(max_length=500)
     answer = models.CharField(max_length=500)
-    media = models.CharField(max_length=500, null=True)
+    media = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.question
