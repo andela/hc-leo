184b26aa72bcb7b4e11eb22f0d2c1502d5039f6f
Jomo <jomomg@gmail.com>
2018-06-20 17:13:18 +0300

    [Feature] Add set priority form

--- hc/accounts/forms.py
+++ hc/accounts/forms.py
@@ -32,3 +32,8 @@ class RemoveTeamMemberForm(forms.Form):
 
 class TeamNameForm(forms.Form):
     team_name = forms.CharField(max_length=200, required=True)
+
+
+class SetPriorityForm(forms.Form):
+    member_email = forms.CharField(max_length=200)
+    priority = forms.CharField(max_length=10)
