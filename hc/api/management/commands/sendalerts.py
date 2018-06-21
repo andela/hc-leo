aef3863cee0b7bcea204f3a19a64e336e13c3ca4
jomomg <jomomg@gmail.com>
2018-06-21 15:31:56 +0300

    [Feature #157694807] Users priority list (#19)
    * [Fix] Fix handle_many to detect checks going from status often to down and send an alert accordingly
    
    * [Feature] Add set priority form
    
    * [Feature] Add priority to the Member model
    
    * [Feature] Add alert_sent and prev_alert_status fields to check model
    
    * [Feature] Add set priority form
    
    * [Feature] Add method to send team notifications
    
    * [Feature] Add save priority functionality to profile view
    
    * [chore] Update migrations
    
    * [chore] Update migrations
    
    * [Feature] Add set priority form
    
    * [Feature] Add method to send team notifications
    
    * [chore] Change dropdown to dropup
    
    * [Fix] Merge conflicting migrations

--- hc/api/management/commands/sendalerts.py
+++ hc/api/management/commands/sendalerts.py
@@ -6,6 +6,8 @@ from django.core.management.base import BaseCommand
 from django.db import connection
 from django.utils import timezone
 from hc.api.models import Check
+from hc.accounts.models import Member
+from hc.lib import emails
 
 executor = ThreadPoolExecutor(max_workers=10)
 logger = logging.getLogger(__name__)
@@ -19,11 +21,14 @@ class Command(BaseCommand):
         query = Check.objects.filter(user__isnull=False).select_related("user")
 
         now = timezone.now()
-        going_down = query.filter(alert_after__lt=now, status="up")
+        going_down_from_up = query.filter(alert_after__lt=now, status="up")
+        going_down_from_often = query.filter(alert_after__lt=now, status="often")
         going_up = query.filter(alert_after__gt=now, status="down")
         nag_on = query.filter(next_nagging__lt=now, nag_status=True, status="down")
         # Don't combine this in one query so Postgres can query using index:
-        checks = list(going_down.iterator()) + list(going_up.iterator()) + list(nag_on.iterator())
+        checks = list(going_down_from_up.iterator()) + list(going_up.iterator()) + \
+            list(going_down_from_often.iterator()) + list(nag_on.iterator())
+
         if not checks:
             return False
 
@@ -63,6 +68,7 @@ class Command(BaseCommand):
         tmpl = "\nSending alert, status=%s, code=%s\n"
         self.stdout.write(tmpl % (check.status, check.code))
         errors = check.send_alert()
+        self.send_team_notification(check)
         if errors is not None:
             for ch, error in errors:
                 self.stdout.write("ERROR: %s %s %s\n" %
@@ -70,7 +76,28 @@ class Command(BaseCommand):
                                 
         connection.close()
         return True
-    
+              
+    def send_team_notification(self, check):
+        """Send notification to members in a team depending on their priority"""
+
+        team = Member.objects.filter(team=check.user.profile).order_by("priority")
+        for member in team:
+            if member.priority == "high" and not member.is_alerted(check):
+                self.notify(check, member.user.email)
+            elif member.priority == "low" and not member.is_alerted(check):
+                self.notify(check, member.user.email)
+        return True
+
+    def notify(self, check, email):
+        """helper function for notifying a user"""
+
+        q = Check.objects.filter(user=check.user)
+        ctx = {"checks": list(q), "check": check}
+        emails.alert(email, ctx)
+        check.alert_sent = True
+        check.prev_alert_status = check.status
+        check.save()
+
     def handle(self, *args, **options):
         self.stdout.write("sendalerts is now running")
 
